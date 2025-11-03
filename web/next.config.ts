65ebe4a011dd92d0719847cf73f48ef436231356
Miracle Kasigwa <miraclekasigwa@gmail.com>
2025-11-03 18:01:47 +0100

    implemented coming soon page

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,19 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
   output: "standalone",
 };
 
-const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
