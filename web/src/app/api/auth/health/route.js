65090c790544b6e477cae198d04f1fbc1bdef41c
Miracle Kasigwa <miraclekasigwa@gmail.com>
2025-11-11 13:53:44 +0100

    deleted un-naissary code

--- web/src/app/api/auth/health/route.js
+++
@@ -1,6 +0,0 @@
-    // app/api/healthz/route.js
-    import { NextResponse } from 'next/server';
-
-    export async function GET() {
-      return NextResponse.json({ status: 'OK' }, { status: 200 });
-    }
